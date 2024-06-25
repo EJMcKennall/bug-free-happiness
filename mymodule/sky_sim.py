73c543174fc4678b3cf13cd4a75c9324a042cf84
Edmund McKennall <ejmckennall@gmail.com>
2024-06-25 14:59:04 +0930

    fixing indentation again

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -4,13 +4,11 @@ Simulate a catalog of stars near to the Andromeda constellation.....
 """
 
 import math
-import random
 
 NSRC = 1_000_000
 
-
 def get_radec():
-     """
+    """
     Generate the ra/dec coordinates of Andromeda
     in decimal degrees.
 
@@ -27,11 +25,12 @@ def get_radec():
     andromeda_dec = '41:16:09'
 
     degrees, minutes, seconds = andromeda_dec.split(':')
-    dec = int(degrees)+int(minutes)/60+float(seconds)/3600
+    dec = int(degrees) + int(minutes) / 60 + float(seconds) / 3600
 
     hours, minutes, seconds = andromeda_ra.split(':')
-    ra = 15*(int(hours)+int(minutes)/60+float(seconds)/3600)
-    ra = ra/math.cos(dec*math.pi/180)
+    ra = 15 * (int(hours) + int(minutes) / 60 + float(seconds) / 3600)
+    ra = ra / math.cos(dec * math.pi / 180)
+    
     return ra, dec
 
 
@@ -42,7 +41,7 @@ def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
 
     Parameters
     ----------
-    ras,decs : list(float)
+    ras, decs : list(float)
         The ra and dec in degrees of the data points
     ref_ra, ref_dec: float
         The reference location
@@ -56,28 +55,34 @@ def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
     ra_out = []
     dec_out = []
     for i in range(len(ras)):
-        if (ras[i]-ref_ra)**2 + (decs[i]-ref_dec)**2 < radius**2:
+        if (ras[i] - ref_ra) ** 2 + (decs[i] - ref_dec) ** 2 < radius ** 2:
             ra_out.append(ras[i])
-            dec_out.append(ras[i])
+            dec_out.append(decs[i])
     return ra_out, dec_out
 
 
-def make_stars(ra,dec, nsrc=NSRC):
-    ...
-    # apply our filter
-    ras, decs = crop_to_circle(ras,decs)
+def make_stars(ra, dec, nsrc=NSRC):
+    # Placeholder implementation
+    ras = [random.uniform(ra - 1, ra + 1) for _ in range(nsrc)]
+    decs = [random.uniform(dec - 1, dec + 1) for _ in range(nsrc)]
+    
+    # Apply our filter
+    ras, decs = crop_to_circle(ras, decs, ra, dec, 1.0)
     return ras, decs
 
 
 def clip_to_radius():
     pass
 
+
 if __name__ == "__main__":
     central_ra, central_dec = get_radec()
     ras, decs = make_stars(central_ra, central_dec)
-    # now write these to a csv file for use by my other program
+    
+    # Write to a CSV file for use by another program
     with open('catalogue.csv', 'w', encoding='utf8') as f:
         print("id,ra,dec", file=f)
         for i in range(NSRC):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+    
     print("Wrote catalogue.csv")
